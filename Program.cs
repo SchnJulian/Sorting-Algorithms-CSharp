31d401ff751dd004c7d0e3b3e088a32c67fe7690
SchnJulian <julian-schnabel@posteo.de>
2019-05-18 17:51:33 +0200

    Erstes Einchecken des Moduls SortingAlgorithms

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SortingAlgorithms {
+    class MainClass {
+        public static void Main(string[] args) {
+            Random random = new Random();
+
+                List<List<int>> A = new List<List<int>>();
+                List<int> x = new List<int>();
+               
+            Tuple<int, int, int> t1 = new Tuple<int, int, int>(2, 3, 5);
+            Tuple<int, int, int> t2 = new Tuple<int, int, int>(5, 3, 1);
+            Tuple<int, int, int> t3 = new Tuple<int, int, int>(0, 3, 5);
+            Tuple<int, int, int> t4 = new Tuple<int, int, int>(0, 0, 1);
+
+            x.Add(1);
+                x.Add(2);
+                x.Add(9);
+
+                printList(Sort.MergeSort(x));
+
+
+
+
+        }
+
+        public static void printList(List<int> list) {
+            foreach (var c in list) {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+
---
+++ Sort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+public interface ITuple { };
+
+namespace SortingAlgorithms {
+    public static class Sort {
+
+        public static List<int> MergeSort(List<int> A) {
+
+            if (A.Count <= 1) return A;
+
+            int n = A.Count - 1;
+            List<int> l = new List<int>(), r = new List<int>();
+
+            int m = A.Count / 2;
+            for (int i = 0; i < m; i++) l.Add(A[i]);
+
+            for (int i = m; i < A.Count; i++) r.Add(A[i]);
+
+            l = MergeSort(l);
+            r = MergeSort(r);
+
+            return Merge(l, r);
+        }
+
+        public static List<int> Merge(List<int> l, List<int> r) {
+            List<int> m = new List<int>();
+            int index_l = 0;
+            int nl = 0, nr = 0;
+            nl = l.Count - 1;
+            nr = r.Count - 1;
+            for (int i = 0; i <= nl + nr + 1; i++) {
+                if (index_l > nl) {
+                    m.Add(r[i - index_l]);
+                    continue;
+                }
+
+                if (index_l < i - nr) {
+                    m.Add(l[index_l]);
+                    index_l++;
+                    continue;
+                }
+
+                if (l[index_l] <= r[i - index_l]) {
+                    m.Add(l[index_l]);
+                    index_l++;
+                } else {
+                    m.Add(r[i - index_l]);
+                }
+            }
+            return m;
+        }
+
+        //public static List<Tuple<int, int, int>> RadixSort(List<Tuple<int,int,int>> A, int r) {
+        //    int n = A.Count;
+        //    List<Tuple<int, int, int>> bucket = new List<Tuple<int, int, int>>();
+        //    for (int i = 0; i < r; i++) {
+        //        for (int j = n - 1; j >= 0; j--) {
+        //            switch (i) {
+        //                case 1
+
+        //                default:
+        //                    break;
+        //            }
+        //            bucket.Insert(A[j].Item1, A[j]);
+        //        }
+        //        A.Clear();
+
+        //        for (int j = 0; j < r; j++) {
+
+        //            A.Add(A[j].bucket[j]ToValueTuple<int, int, int>()
+        //        }
+        //    }
+        //    return A;
+        //}
+    
+        public static List<int> 
+    
+    }
+
+}
