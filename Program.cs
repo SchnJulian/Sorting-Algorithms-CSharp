4f47b44528493f402f3058d9ba28f201d5572a5d
SchnJulian <julian-schnabel@posteo.de>
2019-06-08 12:08:08 +0200

    added gif
    how to use the libary

--- Program.cs
+++ Program.cs
@@ -4,23 +4,8 @@ using SortingAlgorithms;
 namespace SortingAlgorithms {
     class MainClass {
         public static void Main() {
-			var example = new int[] { 17, -20, 100, 33, 200, 1, 0 };
-
-			// Print unsorted array
-			foreach (var item in example) {
-				Console.Write(item + " ");
-			}
-			Console.WriteLine();
-
-			// Sort the array
-			example = Sort.HeapSort(example);
-
-			// Print sorted array
-			foreach (var item in example) {
-				Console.Write(item + " ");
-			}
-			Console.WriteLine();
+			       var example = new int[] { 17, -20, 100, 33, 200, 1, 0 };
+             Sort.IntroSort(example);
 		}
 	}
 }
-
